4ba259599e4609f8fd899eb71ce07559d728b2b1
TVogt23 <tvote24@gmail.com>
2023-01-20 14:03:07 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello, World!\n");
+
+
+
+     this is the new file
+    //Testing
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
